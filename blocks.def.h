93400278a503134ead295a12d477f67db965d720
Jashaswimalya Acharjee <jashaswimalyaacharjee@gmail.com>
2021-04-14 12:51:49 +0530

    changed scripts structure, added unicode characters

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-//        {""   , "~/.config/i3/scripts/weather2"       , 3600, 0},
-        {""   , "~/.config/i3/scripts/network_traffic", 2, 0},
-	{""   , "~/.local/bin/dwmblocks/dwmblocks-net", 2, 0},
-	{""   , "~/.local/bin/dwmblocks/dwmblocks-vol", 2, 10},
-	{"  ", "~/.local/bin/dwmblocks/dwmblocks-ram", 2, 0},
-	{"﬙  ", "~/.local/bin/dwmblocks/dwmblocks-cpu", 2, 0},
-	{""   , "~/.local/bin/dwmblocks/dwmblocks-bat", 2, 0},
-	{"  ", "~/.local/bin/dwmblocks/dwmblocks-time", 5, 0},
+//        {""   , "~/dwmblocks/scripts/weather2"       , 3600, 0},
+        {""   , "~/dwmblocks/scripts/network_traffic", 2, 0},
+        {"  "   , "~/dwmblocks/scripts/fan_speed", 2, 0},
+        {" "   , "~/dwmblocks/scripts/disk", 2, 0},
+        {" "   , "~/dwmblocks/scripts/memory", 2, 0},
+        {""   , "~/dwmblocks/scripts/cpu", 2, 0},
+        {"  : "   , "~/dwmblocks/scripts/temperature", 2, 0},
+        {""   , "~/dwmblocks/scripts/volume", 2, 0},
+        {""   , "~/dwmblocks/scripts/battery_info.sh", 2, 0},
+        {""   , "~/dwmblocks/scripts/clock", 2, 0},
+	//{""   , "~/.local/bin/dwmblocks/dwmblocks-vol", 2, 10},
+	//{"  ", "~/.local/bin/dwmblocks/dwmblocks-ram", 2, 0},
+	//{"﬙  ", "~/.local/bin/dwmblocks/dwmblocks-cpu", 2, 0},
+	//{""   , "~/.local/bin/dwmblocks/dwmblocks-bat", 2, 0},
+	//{"  ", "~/.local/bin/dwmblocks/dwmblocks-time", 5, 0},
 	{"\n", "", 0, 0},
 };
 
